1a0686a43b53021840dc89d5f10e793f502fbbf0
00asdf <53291579+00asdf@users.noreply.github.com>
2022-03-17 01:52:42 +0100

    added some suff

--- src/main/java/at/asdf00/avaplus/init/ItemInit.java
+++ src/main/java/at/asdf00/avaplus/init/ItemInit.java
@@ -1,13 +1,16 @@
 package at.asdf00.avaplus.init;
 
-import at.asdf00.avaplus.objects.items.ItemInfinitySingularity;
+import at.asdf00.avaplus.objects.items.ItemBase;
+import javafx.util.Pair;
+import net.minecraft.client.renderer.block.model.IBakedModel;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class ItemInit {
-    public static final List<Item> ITEM_LIST = new ArrayList<>();
+    public static final List<Item> ITEM_LIST = new LinkedList<>();
 
-    public static final Item INFINITY_SINGULARITY = new ItemInfinitySingularity("infinity_singularity");
+    public static final Item INFINITY_SINGULARITY = new ItemBase("infinity_singularity", 64);
 }
--- src/main/java/at/asdf00/avaplus/proxy/ClientProxy.java
+++ src/main/java/at/asdf00/avaplus/proxy/ClientProxy.java
@@ -3,6 +3,7 @@ package at.asdf00.avaplus.proxy;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
 import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 
 public class ClientProxy extends CommonProxy {
     @Override
